7361edb0a3ec01dfc4b197a260fcd8d227e7e5d6
paullaster <paullasterokoth98@gmail.com>
2022-11-04 13:27:29 +0300

    creating database connection

---
+++ database/database.connection.js
@@ -0,0 +1,17 @@
+//Database connection object:
+//Importing Configuration
+require ('dotenv').config ();
+const option = {
+    client: 'msql',
+    version: '8.0',
+    connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    },
+};
+
+// Import knex
+const knex = require ( 'knex')(option)
