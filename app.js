0f77d164b1adb4aecb3b01db3d8cbe7076d3f07e
paullaster <paullasterokoth98@gmail.com>
2022-11-04 16:56:08 +0300

    adding cors middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  */
 //DEPENDENCIES
 const express = require ('express');
+const cors = require ('cors');
 require ( 'dotenv').config ();
 
 
@@ -16,14 +17,15 @@ const app = express ();
 //APPLICATION SETTINGS
 app.use ( express.json ());
 app.use ( express.urlencoded ( { extended: true}));
+app.use ( cors ())
 
 
 //API
-app.use ( '/api', );
+//app.use ( '/api', );
 
 //LAUNCHING SERVER
 app.listen (process.env.PORT, () => {
-    console.log ( ` Server listening on ${process.env.APP_HOST}:${process.env.PORT}` );
+    console.log ( ` Server listening on ${process.env.APP_HOST}:${process.env.APP_PORT}` );
 });
 
 
