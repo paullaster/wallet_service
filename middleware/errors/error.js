12dec7310dfa483d04122154b25dc376fa3e4076
paullaster <paullasterokoth98@gmail.com>
2022-11-07 15:31:49 +0300

    creating error class

--- middleware/errors/error.js
+++ middleware/errors/error.js
@@ -1,2 +1,27 @@
+//ERROR CLASS:
+class Error {
+    constructor (statusCode, message){
+        this.statusCode = statusCode;
+        this.message = message;
+    };
 
-class Error {};
+    //ERROR CLASS STATIC METHODS:
+    static Forbidden ( message) {
+        return new Error (  403, message);
+    };
+    static NotFound (message) {
+        return new Error (404, message);
+    };
+    static UnAuthorized (message) {
+        return new Error (401, message);
+    };
+    static BadRequest (message) {
+        return new Error (400, message);
+    };
+    static InternalServerError (message) {
+        return new Error (500, message);
+    };
+};
+
+//EXPORT ERROR CLASS:
+module.exports = Error;
