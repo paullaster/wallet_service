76cccc8249bcbaed72cfe00de98725df225f52e0
paullaster <paullasterokoth98@gmail.com>
2022-11-07 10:36:55 +0300

    auth routes

---
+++ controller/auth/account.creation.js
@@ -0,0 +1,10 @@
+//ACCOUNT CREATION FILE:
+
+const createAccount = ( req, res) => {
+  res.json ({
+    message: 'Create Account',
+  })
+};
+
+//EXPORT CREATE ACCOUNT CONTROL FUNCTION:
+module.exports = createAccount;
