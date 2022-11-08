bc459cfece807c47b52ab733a611604287ebc795
paullaster <paullasterokoth98@gmail.com>
2022-11-08 18:53:21 +0300

    handling invalid login

--- controller/auth/login.js
+++ controller/auth/login.js
@@ -2,12 +2,23 @@
 
 
 //INTERNAL DEPENDENCIES:
+const knex = require ( '../../helper/database.connection');
 const login = ( req, res) => {
     const {phonenumber, pin} = req.body;
-
-    res
-    .status (200)
-    .json ({phonenumber, pin})
+    knex ('accounts').where ({accountID: phonenumber})
+    .then ( (rows) => {
+        if ( rows.length < 0) {
+            res
+            .status (400)
+            .json ({
+                message: "Invalid logins"
+            });
+            return;
+        };
+        res
+        .status (200)
+        .json ( rows);
+    })
 };
 //EXPORT LOGIN:
 module.exports = login;
