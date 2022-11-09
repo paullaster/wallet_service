c4d61b7fe02097dfa10295d61d0ae2e12e9b8060
paullaster <paullasterokoth98@gmail.com>
2022-11-09 15:29:10 +0300

    withdraw funds

---
+++ controller/services/withdraw.funds.js
@@ -0,0 +1,16 @@
+//WITHDRAW FUNDS:
+
+
+//DEPENDENCIES:
+
+
+//INTERNAL DEPENDENCIES:
+
+
+//WITHDRAW FUNDS FUNCTION:
+const withdrawFund = (req, res) => {
+    
+};
+
+//EXPORT WITHDRAW FUNDS FUNCTION:
+module.exports = withdrawFund;
--- routes/services/services.routes.js
+++ routes/services/services.routes.js
@@ -1,14 +1,15 @@
 //DEPENDENCIES:
 const servicesRoute = require ( 'express').Router ();
-const {check} = require ( 'express-validator' );
 
 //INTERNAL DEPENDENCIES:
-const accounts = require ( '../../controller/services/fund.accounts');
+const deposit = require ( '../../controller/services/fund.accounts');
 const transfer = require ( '../../controller/services/transfer.funds');
+const withdraw = require ( '../../controller/services/withdraw.funds')
 const verifyToken = require ( '../../middleware/util/verifyToken');
 
-servicesRoute.route ( '/fund/account').post ( verifyToken, accounts);
-servicesRoute.route ( '/fund/transfer').post ( verifyToken, transfer)
+servicesRoute.route ( '/fund/deposit').post ( verifyToken, deposit);
+servicesRoute.route ( '/fund/transfer').post ( verifyToken, transfer);
+servicesRoute.route ( '/fund/withdraw').post (verifyToken, withdraw);
 
 //EXPORT Services Route:
 module.exports = servicesRoute;
