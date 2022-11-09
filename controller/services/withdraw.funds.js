60cbdf475305ff350189b5ec57c1f8751c4b33d2
paullaster <paullasterokoth98@gmail.com>
2022-11-09 15:49:08 +0300

    perforing withdrawal transaction

--- controller/services/withdraw.funds.js
+++ controller/services/withdraw.funds.js
@@ -12,9 +12,28 @@ const withdrawFund = (req, res) => {
     const user = req.user.id;
     knex('accounts').where ( {accountID:user})
     .then ( (rows) => {
+        const {withdrawalAmount} = req.body;
+        if ( typeof withdrawalAmount !== 'number') {
+            res
+            .status (404)
+            .json ( {
+                message: "withdraw amount must be a number",
+            });
+            return;
+        };
+
+        //Perfomring withdrawal transaction:
         
+
+        res.json(rows);
     })
-    res.json(user);
+    .catch ( (error) => {
+        res
+        .status (500)
+        .json ( {
+            error: error.message,
+        });
+    });
 };
 
 //EXPORT WITHDRAW FUNDS FUNCTION:
