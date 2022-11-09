be4cc5086a14df3c479465b1be61ca3459d76b63
paullaster <paullasterokoth98@gmail.com>
2022-11-09 13:05:15 +0300

    fund account service"

--- controller/services/fund.accounts.js
+++ controller/services/fund.accounts.js
@@ -3,11 +3,40 @@
 //INTERNAL DEPENDENCIES:
 const knex = require ( '../../helper/database.connection');
 const accountFunding = (req, res) => {
-    const {amount} =req.body;
+    const user = req.user.id;
+    knex('accounts').where ({accountID: user})
+    .then ( (rows) => {
+        //Getting old account balance:
+        const initialBalance = rows[0].balance;
+        const {amount} =req.body;
 
-    res
-    .status (200)
-    .json (amount)
+        //setting new account balance
+        const newAccountBalance = initialBalance + amount;
+        knex ('accounts').where ( {accountID:rows[0].accountID})
+        .update ( {balance:newAccountBalance})
+        .then ( (data) => {
+            res
+            .status (201)
+            .json ( {
+                message: `${data} ${data > 1 ? 'Items' : 'Item'} updated successfully`,
+            });
+        })
+        .catch ( (error) => {
+            res
+            .status (500)
+            .json ( {
+                error: error.message,
+            });
+        });
+    })
+    .catch ( (err) => {
+        res
+        .status (400)
+        .json ( {
+            error: err.message
+        });
+    });
+
 };
 
 //EXPORT FUND ACCOUNTS:
