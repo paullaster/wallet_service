3221d4c1ce126a64d6a4099fb102245fb9f238ea
paullaster <paullasterokoth98@gmail.com>
2022-11-09 11:14:20 +0300

    authenticate user

--- controller/auth/login.js
+++ controller/auth/login.js
@@ -1,23 +1,29 @@
 //LOGIN:
-
-
+//DEPENDENCIES:
+const bcrypt = require ( 'bcrypt' );
 //INTERNAL DEPENDENCIES:
 const knex = require ( '../../helper/database.connection');
 const login = ( req, res) => {
     const {phonenumber, pin} = req.body;
     knex ('accounts').where ({accountID: phonenumber})
     .then ( (rows) => {
-        if ( rows.length < 0) {
+        if(rows.length === 0) {
             res
             .status (400)
             .json ({
-                message: "Invalid logins"
+                message: "Invalid account"
             });
             return;
         };
-        // res
-        // .status (200)
-        // .json ( rows);
+        bcrypt.compare (pin, rows[0].pin)
+        .then ( (val) => {
+            if ( !val) {
+                res
+                .status (400)
+                .json ( {message: "Incorrect password"});
+                return;
+            };
+        })
     })
 };
 //EXPORT LOGIN:
